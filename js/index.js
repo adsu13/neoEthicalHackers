1957fc290b8e349483978090b004583321e873dc
Adson Pereira <adsongarapinha@gmail.com>
2023-07-09 05:56:53 -0300

    c

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     var header = document.querySelector('header');
     var scrollPosition = window.scrollY;
-  
+
     if (scrollPosition > 0) {
-      header.classList.add('menu-black');
+        header.classList.add('menu-black');
     } else {
-      header.classList.remove('menu-black');
+        header.classList.remove('menu-black');
     }
-  });
-  
+});
+const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+const menuList = document.querySelector('.menu ul');
+
+mobileMenuIcon.addEventListener('click', function () {
+    menuList.classList.toggle('active');
+});
